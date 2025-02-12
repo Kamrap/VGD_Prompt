7b7519558cd039e7332ad41c1e8d6dfab0512f81
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-02-12 09:09:52 -0700

    Create Feb_12.cs

---
+++ Feb_12.cs
@@ -0,0 +1,7 @@
+//This is my code for feb 12
+//We learned about different character types
+//code:
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("true");
+Console.WriteLine(true);
