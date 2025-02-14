b9d3b0832a189c46718d2b40555e7e2c75f509b1
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-02-14 12:58:26 -0700

    Update Feb14.cs

--- Feb14.cs
+++ Feb14.cs
@@ -1,2 +1,10 @@
 // this is my journal for Feb 14
 //
+string firstName = ("hello");
+Console.WriteLine(firstName);
+
+int number = (123);
+Console.WriteLine(number);
+
+float numberss = (0.12f);
+Console.WriteLine(numberss);
