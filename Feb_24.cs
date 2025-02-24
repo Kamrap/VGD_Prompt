f8420a9ddb0ab496166591be571e599a3deaacc8
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-02-24 14:59:30 -0700

    Create Feb_24.cs

---
+++ Feb_24.cs
@@ -0,0 +1,20 @@
+//My journal for February 24, 2025 unit 5 and 6
+//I learned about variables and how you can store data in them by initializing them.
+
+//unit 5 code:
+var message = "Hello World!";
+message = "hello";
+Console.WriteLine(message);
+
+// unit 6 code:
+var name = ("Bob! ");
+var mail = (3);
+var temp = (34.4m);
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("You have ");
+Console.Write(mail);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temp);
+Console.Write(" celsius.");
