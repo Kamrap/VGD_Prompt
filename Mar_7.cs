09e2692706f243cd522bd80e52761ba3ac4e4b29
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-03-07 10:27:23 -0700

    Create Mar_7.cs

---
+++ Mar_7.cs
@@ -0,0 +1,16 @@
+//This is my journal prompt for March 7
+// Today I learnt how to define different values with he correct variable type. For example, to define a number, you would have to use the int variable, and not the string or float. A float is used for a number, but it is used for a number with a decimal.
+
+//code:
+string Name = ("Bob");
+int Num = 3;
+float Numd = 34.4f;
+Console.Write("Hello, ");
+Console.Write(Name);
+Console.Write("! You have ");
+Console.Write(Num);
+Console.Write(" messages in you inbox. The temperature is ");
+Console.Write(Numd);
+Console.Write("celsius.");
+
+//output: Hello, Bob! You have 3 messages in you inbox. The temperature is 34.4celsius.
