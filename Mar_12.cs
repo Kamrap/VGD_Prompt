a715981f1ada6ba333c2064e86a0e1d773d9d3ca
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-03-12 08:58:52 -0600

    Create Mar_12.cs

---
+++ Mar_12.cs
@@ -0,0 +1,6 @@
+// This is my journal for Mar 12
+//I learnt about the $ sign, and what its function is.
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
