444eff1566830d54b3c007026868352a9792e126
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-03-12 09:06:08 -0600

    Update Mar_5.cs

--- Mar_5.cs
+++ Mar_5.cs
@@ -1,3 +1,6 @@
+//This is my journal for Mar 5.
+//I learnt how to print different types of strings. For example, you can print only one character using the single quotation marks.
+
 Console.WriteLine("123");
 Console.WriteLine(123);
 
