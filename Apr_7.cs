f38ddc2c497489b590bf6fdcbc8ba272fbc6dff6
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-04-07 13:46:58 -0600

    Apr_7.cs

---
+++ Apr_7.cs
@@ -0,0 +1,10 @@
+// My journal for April 7
+// I learnt how you can use math in C#.
+
+// code
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+// output:
+// Bob sold 14 widgets.
