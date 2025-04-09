c4111fa1d2ace077e6a5f55f3df25b784ca7cc43
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-04-09 08:59:01 -0600

    Create Apr_9.cs

---
+++ Apr_9.cs
@@ -0,0 +1,11 @@
+// This is my journal for April 9
+// Today I learnt the function of the "%", and how to use it and how it works. I also learnt how to use basics msth operations like +, -, *, and / in C#.
+
+// code:
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
