a0f367a18d0b34330ced4049b447bc28db93f41f
Kamrap <86315838+Kamrap@users.noreply.github.com>
2025-04-11 10:05:50 -0600

    Create Apr_11.cs

---
+++ Apr_11.cs
@@ -0,0 +1,7 @@
+// This is my journal for April 11.
+//  I learnt how to make a fahrenheit to celsius convertion tool.
+
+//code:
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32) * (5m / 9m);
+Console.WriteLine($"The temperature is {celsius}  Celsius.");
